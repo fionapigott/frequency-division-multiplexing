163a0f1fbc7ab1520342e70ef5a1c2b2dad8250d
Nick Vanderweit <nick.vanderweit@gmail.com>
2012-12-09 13:04:57 -0700

    Add comments to split and stack

--- src/split.m
+++ src/split.m
@@ -1,10 +1,20 @@
+% Function to split a signal previously created by stack into its constituent
+% signals.
+% Arguments:
+%     signal: The composite signal created by stack
+%     n:      The number of output signals we expect to get back
+% Returns: An m by n array of signals, where m is the number of samples in each
 function retval = split(signal, n)
     transformed = fft(signal);
     L = length(transformed);
 
+    % Take out the redundant information from the signal
     reduced = transformed(1:L/2+1);
 
+    % Split the remaining information into n different signals
     split = reshape(reduced, length(reduced) / n, n);
+
+    % Reconstruct the previously discarded information to make the ifft real
     mirrored = flipud(conj(split(2:length(split)-1,:)));
     retval = real(ifft([split; mirrored]));
 endfunction
--- src/stack.m
+++ src/stack.m
@@ -1,3 +1,12 @@
+% A function to concatenate two REAL signals in frequency and return a new
+% signal containing all those frequencies. This is done by taking the DFT of
+% each input signal, stripping away the redundant frequencies, concatenating
+% the remaining frequencies, and then adding redundant frequencies back in to
+% satisfy the reality condition. Finally, the ifft is taken to get a real
+% signal back.
+% Arguments:
+%     signals: An m by n array containing n different signals with m samples
+% Returns: A new signal with all frequency information from the input signals.
 function retval = stack(signals)
     % The FFT is usually done with a divide-and-conquer algorithm that will
     % run faster for a very composite number of samples, so we get a power of
